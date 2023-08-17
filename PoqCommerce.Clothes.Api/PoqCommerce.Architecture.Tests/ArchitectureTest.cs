9912924fdb84bd35bcd6ef8a16c8b0327634700e
Georgi Bozhilov <talic@abv.bg>
2023-08-17 16:21:38 +0300

    Implement Architecture tests

---
+++ PoqCommerce.Clothes.Api/PoqCommerce.Architecture.Tests/ArchitectureTest.cs
@@ -0,0 +1,102 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+using System.Reflection;
+
+namespace PoqCommerce.Architecture.Tests
+{
+    public class ArchitectureTest
+    {
+        private const string ApplicationNamespace = "PoqCommerce.Application";
+        private const string DomainNamespace = "PoqCommerce.Domain";
+        private const string PresentationNamespace = "PoqCommerce.Api";
+        private const string InfrastructureNamespace = "PoqCommerce.Mocky.Io";
+
+        [Fact]
+        public void Domain_Should_Not_HaveDependencyOnOtherProjects()
+        {
+            // Arrange
+            Assembly assembly = Assembly.Load(DomainNamespace);
+
+            var otherProjects = new[] {
+                ApplicationNamespace,
+                PresentationNamespace,
+                InfrastructureNamespace,
+            };
+
+            // Act
+            var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+            //Assert
+            testResult.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Application_Should_Not_HaveDependencyOnOtherProjects()
+        {
+            // Arrange
+            Assembly assembly = Assembly.Load(ApplicationNamespace);
+
+            var otherProjects = new[] {
+                PresentationNamespace,
+                InfrastructureNamespace,
+            };
+
+            // Act
+            var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+            //Assert
+            testResult.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Presentation_Should_HaveDependencyOnOtherProjects()
+        {
+            // Arrange
+            Assembly assembly = Assembly.Load(PresentationNamespace);
+
+            var otherProjects = new[] {
+                ApplicationNamespace,
+                InfrastructureNamespace
+            };
+
+            // Act
+            var testResult = Types
+            .InAssembly(assembly)
+            .Should()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+            //Assert
+            testResult.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Services_Should_Have_DependencyOnDomain()
+        {
+            // Arrange
+            Assembly assembly = Assembly.Load(ApplicationNamespace);
+
+            // Act
+            var testResult = Types
+            .InAssembly(assembly)
+            .That()
+            .AreClasses()
+            .And()
+            .HaveNameEndingWith("Service")
+            .Should()
+            .HaveDependencyOn(DomainNamespace)
+            .GetResult();
+
+            // Assert
+            testResult.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
