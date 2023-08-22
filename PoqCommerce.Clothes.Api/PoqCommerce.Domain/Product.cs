f20b7227855ba94f6eb76607f0417c728094f860
Georgi Bozhilov <talic@abv.bg>
2023-08-22 14:29:39 +0300

    Remove Id

--- PoqCommerce.Clothes.Api/PoqCommerce.Domain/Product.cs
+++ PoqCommerce.Clothes.Api/PoqCommerce.Domain/Product.cs
@@ -2,7 +2,6 @@
 {
     public class Product
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public double Price { get; set; }
         public List<string> Sizes { get; set; }
