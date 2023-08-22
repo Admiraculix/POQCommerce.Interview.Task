04f8324259b5133dc87290921989673100ba3317
Georgi Bozhilov <talic@abv.bg>
2023-08-22 16:39:15 +0300

    Add missing registration of factory

--- PoqCommerce.Clothes.Api/PoqCommerce.Application/Extensions/DependencyRegistration.cs
+++ PoqCommerce.Clothes.Api/PoqCommerce.Application/Extensions/DependencyRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PoqCommerce.Application.Factories;
 using PoqCommerce.Application.Interfaces;
 
 namespace PoqCommerce.Application.Extensions
@@ -8,8 +9,8 @@ namespace PoqCommerce.Application.Extensions
     {
         public static IServiceCollection RegisterApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddTransient<IFilterObjectDtoFactory, FilterObjectDtoFactory>();
             services.AddTransient<IProductService, ProductService>();
-
             return services;
         }
     }
